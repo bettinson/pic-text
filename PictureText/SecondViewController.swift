241d786ddddb2260da3b0a84f7bf60fa9af08659
bettinson <mattbettinson@me.com>
2016-07-19 22:43:48 +0200

    Initial Commit

---
+++ PictureText/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PictureText
+//
+//  Created by Matt Bettinson on 2016-07-19.
+//  Copyright © 2016 Matt Bettinson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
